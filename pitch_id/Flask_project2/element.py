a6be54a86560400d84175cfb266cf95a25cbf618
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-11 23:10:00 +0800

    Delete pitch_id/Flask_project2 directory

--- pitch_id/Flask_project2/element.py
+++
@@ -1,4 +0,0 @@
-note_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-accidentals = ['Flat (♭)', 'Natural (♮)', 'Sharp (♯)']
-
-accidentals_lilypond = {'Sharp (♯)':"is",'Natural (♮)':"",'Flat (♭)':"es"}
