2b412e128030197be8285065f123e16cb110bf93
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-11 23:02:17 +0800

    Update topics.py

--- instrument_knowledge_quiz/topics.py
+++ instrument_knowledge_quiz/topics.py
@@ -1,4 +1,4 @@
-from question import reed,transposing,clef,voice_types,piano_knowledge,orniments,instrumental_technique
+from instrument_knowledge_quiz.question import reed,transposing,clef,voice_types,piano_knowledge,orniments,instrumental_technique
 def pick_topic(pick = str):
     topic = {'Reed':reed(),'Transposing':transposing(),
              "Clef":clef(),"Voice types":voice_types(),
