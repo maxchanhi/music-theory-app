044a4759028dacf9c21de1522598bce35d16eeec
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-12 17:54:41 +0800

    Update main.py

--- melody_key/chord_progression/main.py
+++ melody_key/chord_progression/main.py
@@ -43,8 +43,8 @@ def chord_progression_main():
     
     print("question_data",ss["question_data"])
     st.image("melody_key/chord_progression/static/cropped_score_question.png", use_column_width=True)
-    user_ans = st.radio(f"Select the correct harmonic pattern in {ss["question_data"]["key_signature"]}:",ss["question_data"]["dis_options"])
-
+    user_ans = st.radio(f"Select the correct harmonic pattern in {ss['question_data']['key_signature']}:", ss["question_data"]["dis_options"])
+    
     if st.button("Submit",on_click=button_pressed,disabled=ss["button_pressed_chord"]):
         if options.index(user_ans) == correct_index:
             emoji= random.choice(fun_emoji_list)
