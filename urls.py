8c01a6e3edf5ff3c6023379b872581132ecc5f57
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-12 10:01:03 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from rhythm_generation.grouping_quiz.main import grouping_quiz_main
+from grouping_quiz.main import grouping_quiz_main
 from instrument_knowledge_quiz import knowledgemain
 from compound_simple_time.main import compound_simple_main
 from interval.interval_quiz import interval_main
