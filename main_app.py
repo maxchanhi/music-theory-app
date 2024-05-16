d30d05db9cdd1834700df0187f66e6a1bcf4564f
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-16 12:18:25 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,5 +1,29 @@
+from grouping_quiz.main import grouping_quiz_main
+from instrument_knowledge_quiz.music_quiz import knowledgemain
+from compound_simple_time.main import compound_simple_main
+from interval.interval_quiz import interval_main
+from melody_key.find_key_main import melody_key_main
+from pitch_id.id import pitch_main
+from melody_key.chord_progression.main import chord_progression_main
+from main_app import intro,main
 import streamlit as st
-from urls import fun_emoji_list, page_names_to_funcs
+
+fun_emoji_list = [
+    "😂",  "🎉",   "🚀",  "🐱", 
+    "🐶",  "🦄",  
+    "🎶",  "😱","👼🏻","💃🏻","🐰","🐒","🐣","🦀","💥","✨","🥳",
+    "🍦",  "🌟",  "👻",  
+    "🎈",   "🎮",  "💩"
+]
+page_names_to_funcs = {
+    "—": intro,
+    "Grouping and beaming": grouping_quiz_main,"Simple-compound Modulation": compound_simple_main,
+    "Pitch Identification": pitch_main,"Interval": interval_main,
+    "Identifying Key in a Melody": melody_key_main,"Chord Progression": chord_progression_main,
+    "Instrumental Knowledge": knowledgemain,
+}
+import streamlit as st
+
 import random
 #from urls import page_names_to_funcs
 def intro():
