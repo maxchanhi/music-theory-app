5aad9e5c58f1cff7d02a62c3c6b730c594afe6c4
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-17 11:39:31 +0800

    bio added

--- pages/2_about_me.py
+++ pages/2_about_me.py
@@ -6,5 +6,5 @@ if st.button("Home"):
 st.title("About me😄")
 st.write("Hi there, I am Max. I began my musical journey playing the flute and pursued both my Bachelor's and Master's degrees at Hong Kong Baptist University. During my undergraduate studies, I took up the bassoon as my second instrument, flute was my major instrument, that is quite important. After graduating from HKBU, I pursued a Master of Composition degree at the Royal Birmingham Conservatoire. I also taught myself Python so I can hop on the AI hype train. I am now back in Hong Kong, developing an app while start learning the clarinet.")
 st.write("Huge thanks to my composition teachers in UK: Andrew Toovey and Ed Bennett. My flute teacher: Brian Chan and Olivier Nowak. My bassoon teacher: Angel Cheung")
-st.success("For more feedback, information or lessons, please contact me at: chakhangc@yahoo.com.hk. Also see my YouTube channel: https://www.youtube.com/channel/UCeJw45clCLOABqXxKhHoAlA")
+st.success("For more feedback, information or lessons, please contact me at: chakhangc@yahoo.com.hk. Also see my YouTube channel: https://www.youtube.com/channel/UCeJw45clCLOABqXxKhHoAlA . My Github repo:https://github.com/maxchanhi . ")
 st.caption("A huge credit goes to my girlfriend, Phoebe, who has been a tremendous support and has provided many fun ideas.")
