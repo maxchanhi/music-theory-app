dd4040949e629014514725306600abddc94ff594
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-24 11:42:17 +0800

    Add files via upload

--- instrument_knowledge_quiz/create_faiss_index.py
+++ instrument_knowledge_quiz/create_faiss_index.py
@@ -14,7 +14,7 @@ loader = TextLoader(DOC_PATH)
 pages = loader.load()
 
 # Split the doc into smaller chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 chunks = text_splitter.split_documents(pages)
 
 # Get OpenAI Embedding model
@@ -26,4 +26,4 @@ db_faiss = FAISS.from_documents(chunks, embeddings)
 # Save the FAISS index to disk
 db_faiss.save_local(INDEX_PATH)
 
-st.write("Index saved to disk.")
+print("Index created successfully!")
