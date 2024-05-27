dcf72a8b8b6696cc622e59233a8f62e9605facc0
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-27 09:35:50 +0800

    Add files via upload

---
+++ duration_equation/cal_main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from random import randint
+from fractions import Fraction
+from question import generate_question, correct_ans,question_elements
+def duration_cal_main():
+    st.title("Music Duration Quiz")
+    col1,col2,col3=st.columns(3)
+    with col1:
+        hard_mode=st.checkbox("Hard mode")    
+    with col2:
+        simple_dotted=st.checkbox("Dotted",value=True)
+    with col3:
+        double_dotted=st.checkbox("Double dotted")
+
+    if 'question' not in st.session_state:
+        duration,dotted_vartion=question_elements(hard_mode,simple_dotted,double_dotted)
+        small_dur, question_dur, question_dotted,dotted_time = generate_question(duration,dotted_vartion)
+        st.session_state['question'] = (small_dur, question_dur, question_dotted, dotted_time)
+        st.session_state['correct'] = correct_ans(small_dur, question_dur, dotted_time)
+        st.session_state['pressed_cd']=False
+    small_dur, question_dur, question_dotted, dotted_time = st.session_state['question']
+    next_question=st.button("Next Question")
+    st.subheader(f"How many {small_dur}s equal to a {question_dotted}{question_dur}?")
+
+    user_ans = st.text_input("Enter your answer:",max_chars=2)
+
+    if st.button("Submit",disabled=st.session_state['pressed_cd']):
+        correct = st.session_state['correct']
+        st.session_state['pressed_cd']=True
+        if str(user_ans) == str(correct):
+            st.success("Correct answer")
+        else:
+            st.error(f"Wrong answer, the answer is {correct}.")
+
+    if next_question and st.session_state['pressed_cd']:
+        st.session_state['pressed_cd']=False
+        duration,dotted_vartion=question_elements(hard_mode,simple_dotted,double_dotted)
+        small_dur, question_dur, question_dotted,dotted_time = generate_question(duration,dotted_vartion)
+        st.session_state['question'] = (small_dur, question_dur, question_dotted, dotted_time)
+        st.session_state['correct'] = correct_ans(small_dur, question_dur, dotted_time)
+        st.rerun()
+
+if __name__ == "__main__":
+    duration_cal_main()
---
+++ duration_equation/question.py
@@ -0,0 +1,57 @@
+import time
+from random import randint
+from fractions import Fraction
+hard_duration = ["hemidemisemiquacer","demisemiquaver", "semiquaver","quaver","crotchet","minim","semibreve","breve"]
+easy_duration=["semiquaver","quaver","crotchet","minim","semibreve"]
+
+duration_fration={"hemidemisemiquaver":Fraction(1,64),"demisemiquaver":Fraction(1,32), "semiquaver":Fraction(1,16), "quaver":Fraction(1,8), "crotchet":Fraction(1,4), "minim":Fraction(1,2), "semibreve":1, "breve":2}
+
+dotted_duration=[1,Fraction(3,2),Fraction(7,4)]
+
+def question_elements(hard=bool,simple=bool,double_dotted=bool):
+    if hard:
+        duration = hard_duration
+    else:
+        duration = easy_duration
+    dotted_vartion=[" "]
+    if simple:
+        dotted_vartion.append("dotted ")
+    if double_dotted:
+        dotted_vartion.append('double-dotted ')
+    
+    return duration, dotted_vartion
+
+def generate_question(duration,dotted_vartion):
+    question_idx = randint(3, len(duration) - 1)  # Corrected the range to avoid IndexError
+    question_dur = duration[question_idx]
+    dotted_idx = randint(0,len(dotted_vartion)-1)
+    question_dotted = dotted_vartion[dotted_idx]
+    dotted_time = dotted_duration[dotted_idx]
+    small_idx = question_idx // randint(2, 4)
+    small_dur = duration[small_idx]
+    #print (f"How many {small_dur}s equal to a {question_dotted}{question_dur}?")
+    print(small_dur, question_dur,question_dotted,dotted_time)
+    return small_dur, question_dur,question_dotted,dotted_time
+
+def user_input():
+    user_ans = input('Enter your answer: ')
+    return user_ans
+
+def correct_ans(small,big,dotted_time):
+    correct=(duration_fration[big]*dotted_time)//duration_fration[small]
+    return correct
+
+def check_ans(ans, correct):
+    if str(ans) == str(correct):
+        print("Correct answer")
+    else:
+        print("Wrong answer")
+
+def main():
+    small_dur, question_dur,dotted_time = generate_question()
+    correct = correct_ans(small_dur, question_dur,dotted_time)
+    result = user_input()
+    check_ans(result, correct)
+
+if __name__ == "__main__":
+    main()
