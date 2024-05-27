5f8f865f738c119c7db71ff1b3f84e2c6808b1c3
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-27 09:47:18 +0800

    Update cal_main.py

--- duration_equation/cal_main.py
+++ duration_equation/cal_main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from random import randint
 from fractions import Fraction
-from question import generate_question, correct_ans,question_elements
+from duration_equation.question import generate_question, correct_ans,question_elements
 def duration_cal_main():
     st.title("Music Duration Quiz")
     col1,col2,col3=st.columns(3)
@@ -12,20 +12,20 @@ def duration_cal_main():
     with col3:
         double_dotted=st.checkbox("Double dotted")
 
-    if 'question' not in st.session_state:
+    if 'questioncd' not in st.session_state:
         duration,dotted_vartion=question_elements(hard_mode,simple_dotted,double_dotted)
         small_dur, question_dur, question_dotted,dotted_time = generate_question(duration,dotted_vartion)
-        st.session_state['question'] = (small_dur, question_dur, question_dotted, dotted_time)
-        st.session_state['correct'] = correct_ans(small_dur, question_dur, dotted_time)
+        st.session_state['questioncd'] = (small_dur, question_dur, question_dotted, dotted_time)
+        st.session_state['correctcd'] = correct_ans(small_dur, question_dur, dotted_time)
         st.session_state['pressed_cd']=False
-    small_dur, question_dur, question_dotted, dotted_time = st.session_state['question']
+    small_dur, question_dur, question_dotted, dotted_time = st.session_state['questioncd']
     next_question=st.button("Next Question")
     st.subheader(f"How many {small_dur}s equal to a {question_dotted}{question_dur}?")
 
     user_ans = st.text_input("Enter your answer:",max_chars=2)
 
     if st.button("Submit",disabled=st.session_state['pressed_cd']):
-        correct = st.session_state['correct']
+        correct = st.session_state['correctcd']
         st.session_state['pressed_cd']=True
         if str(user_ans) == str(correct):
             st.success("Correct answer")
@@ -36,8 +36,8 @@ def duration_cal_main():
         st.session_state['pressed_cd']=False
         duration,dotted_vartion=question_elements(hard_mode,simple_dotted,double_dotted)
         small_dur, question_dur, question_dotted,dotted_time = generate_question(duration,dotted_vartion)
-        st.session_state['question'] = (small_dur, question_dur, question_dotted, dotted_time)
-        st.session_state['correct'] = correct_ans(small_dur, question_dur, dotted_time)
+        st.session_state['questioncd'] = (small_dur, question_dur, question_dotted, dotted_time)
+        st.session_state['correctcd'] = correct_ans(small_dur, question_dur, dotted_time)
         st.rerun()
 
 if __name__ == "__main__":
