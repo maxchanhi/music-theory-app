ef4fc1aba01407ed360fd3ff49c0460db35d3f60
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-06 21:29:00 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -85,7 +85,7 @@ def intro():
                 support_url = "https://music-glossary.streamlit.app"
                 # Use JavaScript to automatically click the link
 
-                st.markdown(f'<meta http-equiv="refresh"; url={support_url}">', unsafe_allow_html=True)
+                st.markdown(f'''<meta http-equiv="refresh"; url={support_url}">''', unsafe_allow_html=True)
 
                 st.markdown("[Music Dictionary](https://music-glossary.streamlit.app)")
 
