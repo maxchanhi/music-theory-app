5ccbca050fffb86b574de28afce073123057bb60
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-16 17:07:17 +0800

    Update needle_main.py

--- grouping_quiz/needle_main.py
+++ grouping_quiz/needle_main.py
@@ -20,31 +20,30 @@ def grouping_quiz_main():
         ss["press_idx_gp"] = None
 
     st.title("Grouping and beaming quiz")
-
-    if st.button("New Question") and ss["check_ans"]:
-        ss["grouping_link"] = needle_in_haystack()
-        ss["check_ans"] = False
-        ss["press_idx_gp"] = None
-
     if len(ss["grouping_link"]) > 0:
         for idx, link in enumerate(ss["grouping_link"]):
             col_1, col_2 = st.columns([5, 1])
             with col_1:
                 st.image(link)
-
             with col_2:
                 st.write("    ")
                 st.button(f"Option {idx+1}", key=idx, on_click=button_pressed_gp, args=(idx,), disabled=idx == ss["press_idx_gp"])
-
-    if st.button("Check answer", disabled=ss["check_ans"]) and ss["press_idx_gp"] is not None:
-        ss["check_ans"] = True
-        if "Correct_rest" in ss["grouping_link"][ss["press_idx_gp"]]:
-            st.success("Correct")
-            rain_emoji()
-        else:
-            correct_idx = ss["press_idx_gp"]
-            st.warning(f"Wrong. The answer is option {correct_idx}")
-        ss["press_idx_gp"] = None
+    col_1, col_2 = st.columns([5, 1])
+    with col_1:
+        if st.button("New Question",disabled= not ss["check_ans"]) and ss["check_ans"]:
+            ss["grouping_link"] = needle_in_haystack()
+            ss["check_ans"] = False
+            ss["press_idx_gp"] = None
+    with col_2:
+        if st.button("Check answer", disabled=ss["check_ans"]) and ss["press_idx_gp"] is not None:
+            ss["check_ans"] = True
+            if "Correct_rest" in ss["grouping_link"][ss["press_idx_gp"]]:
+                st.success("Correct")
+                rain_emoji()
+            else:
+                correct_idx = ss["press_idx_gp"]
+                st.warning(f"Wrong. The answer is option {correct_idx}")
+            ss["press_idx_gp"] = None
 
 if __name__ == "__main__":
     grouping_quiz_main()
