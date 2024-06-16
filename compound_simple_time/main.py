8cab335cd4333c9b2541325073cd376591fccfcb
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-16 17:43:24 +0800

    Update main.py

--- compound_simple_time/main.py
+++ compound_simple_time/main.py
@@ -1,19 +1,15 @@
-
+from compound_simple_time.melody import main_generate
+from urls import disclaimer,rain_emoji
+import asyncio,random
+from compound_simple_time.asynchronous import score_generation
+def submit_pressed():
+    st.session_state.submit_pressed_com = True
+def select_option(idx):
+    if st.session_state.option_selected is not None:
+        st.session_state[f"disabled_{st.session_state.option_selected}"] = False
+    st.session_state.option_selected = idx
+    st.session_state[f"disabled_{idx}"] = True
 def compound_simple_main():
-
-    def select_option(idx):
-        if st.session_state.option_selected is not None:
-            st.session_state[f"disabled_{st.session_state.option_selected}"] = False
-        st.session_state.option_selected = idx
-        st.session_state[f"disabled_{idx}"] = True
-    from compound_simple_time.melody import main_generate
-    from compound_simple_time.notation import fun_emoji_list
-    from urls import disclaimer
-    import asyncio,random
-    from compound_simple_time.asynchronous import score_generation
-    import streamlit as st
-    from streamlit_extras.let_it_rain import rain
-
     if 'option_selected' not in st.session_state:
         st.session_state.option_selected = []
     
@@ -22,15 +18,10 @@ def compound_simple_main():
         st.session_state.submit_pressed_com = False
         asyncio.run(score_generation(st.session_state.question_data_com))
     
-
     question_data = st.session_state.question_data_com
     st.title("Compound-simple-time Modulation Quiz")
     st.subheader(question_data['question'])
-    if st.button("Generate New Question") and st.session_state.submit_pressed_com:
-        st.session_state.question_data_com = main_generate()
-        asyncio.run(score_generation(st.session_state.question_data_com))
-        st.session_state.submit_pressed_com = False
-        st.rerun()
+    
     st.image("compound_simple_time/temp/cropped_score_question_melody.png", caption='Question')
 
     for idx, option in enumerate(question_data['options']):
@@ -48,10 +39,18 @@ def compound_simple_main():
             args=(idx,),
             disabled=st.session_state[f"disabled_{idx}"]
         )
-    def submit_pressed():
-        st.session_state.submit_pressed_com = True
+
     pressed= st.session_state.submit_pressed_com
-    if st.button("Submit Answer",on_click=submit_pressed,disabled=pressed):
+    col_1,col_2=st.columns([5,1])
+    with col_1:
+        if st.button("New Question",on_click=submit_pressed,disabled= not pressed ) and st.session_state.submit_pressed_com:
+            st.session_state.question_data_com = main_generate()
+            asyncio.run(score_generation(st.session_state.question_data_com))
+            st.session_state.submit_pressed_com = False
+            st.rerun()
+    with col_2:
+        check_ans_mm=st.button("Check Answer",on_click=submit_pressed,disabled=pressed)
+    if check_ans_mm and st.session_state.option_selected is not None:
         correct_idx = None
         for idx, (time, melody) in enumerate(question_data["options"]):
             if time == question_data["answer"][0] and melody == question_data["answer"][1]:
@@ -59,9 +58,7 @@ def compound_simple_main():
                 break
 
         if st.session_state.option_selected == correct_idx:
-            fun_emoji = random.choice(fun_emoji_list)
-            st.success(f"Correct! {fun_emoji}")
-            rain(emoji=fun_emoji, animation_length="1")
+            rain_emoji()
         else:
             st.error(f"Wrong answer! The correct answer is Option {correct_idx + 1}")
 
