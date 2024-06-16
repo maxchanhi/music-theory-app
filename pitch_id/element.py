b82d1356001744e3ae9751714d66d79c9735cb25
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-16 16:52:26 +0800

    Update element.py

--- pitch_id/element.py
+++ pitch_id/element.py
@@ -64,16 +64,6 @@ def ranged_score_generation(chosen_clef,chosen_note,chosen_range):
 
     subprocess.run(['lilypond', '-dpreview', '-dbackend=eps', '--png', '-dresolution=300', '--output=score', 'score.ly'],
                    check=True)
-  # Open the generated PNG file
-    with Image.open('score.png') as img:
-      # Calculate the crop rectangle
-      width, height = img.size
-      crop_height = height // 10 
-      crop_rectangle = (120, 0, 350, crop_height)
-      
-      # add code to crop 1/10 from the top
-      cropped_img = img.crop(crop_rectangle)
-      cropped_img.save(f'cropped_score.png')
 
 def get_note(available_clef=["treble","bass","tenor","alto"],
              accidentals=accidentals,
