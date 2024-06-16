f646506a118bb71ec57c315338c0b46fd01f31a8
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-16 16:40:10 +0800

    Add files via upload

--- pitch_id/element.py
+++ pitch_id/element.py
@@ -1,4 +1,93 @@
+import streamlit as st
+import random
+import subprocess
+from PIL import Image
 note_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 accidentals = ['Flat (♭)', 'Natural (♮)', 'Sharp (♯)','Double-sharp (x)','Double-flat (𝄫)']
 
 accidentals_lilypond = {'Sharp (♯)':"is",'Natural (♮)':"",'Flat (♭)':"es",'Double-sharp (x)':"isis",'Double-flat (𝄫)':"isis"}
+levels={"basic":[["treble","bass"],['Natural (♮)']],
+        "intermediate":[["treble","bass"],['Flat (♭)', 'Natural (♮)', 'Sharp (♯)']],
+        "advanced":[["treble","bass"],accidentals],
+        "c clefs":[["alto","tenor"],['Natural (♮)']],"all clefs":[["treble","alto","tenor","bass"],accidentals]}
+clefs = {
+    "treble":{
+        "instaff": ['d', 'e', 'f', 'g', 'a', 'b', "c'", "d'", "e'", "f'", "g'"],
+        "leger_lines": ["g,", "a,", "b,", "c", "a'", "b'", "c''", "d''"], "pitch_range":"c'"
+    },
+    "bass": {
+        "instaff": ['f,', 'g,', 'a,', 'b,', "c", "d", "e", "f", "g","a","b"],
+        "leger_lines": ["c,", "d,", "e,", "c'", "d'","e'","f'"], "pitch_range":"c"
+    },
+    "tenor":{
+        "instaff": ["c,",'d,', 'e,', 'f,', 'g,', 'a,', 'b,', "c", "d", "e", "f"],
+        "leger_lines": ["g,,", "a,,", "b,,", "g", "a", "b", "c''"], "pitch_range":"c'"
+    },
+    "alto": {
+        "instaff": ['e,', 'f,', 'g,', 'a,', 'b,', "c", "d", "e", "f","g",'a'],
+        "leger_lines": ["c,",'d,', "b", "c'","d'","e'"], "pitch_range":"c'"
+    }
+}
+
+
+#print(get_note(leger_line=True))
+def ranged_score_generation(chosen_clef,chosen_note,chosen_range):
+    lilypond_score = f"""
+  \\version "2.24.3"  % ensure this matches your LilyPond version
+  \\header {{
+    tagline = ""  % removes the default LilyPond tagline
+  }}
+  #(set-global-staff-size 26)  % Adjust staff size to affect resolution
+
+  \\score {{
+    {{
+      \\clef {chosen_clef}
+      \\fixed {chosen_range}
+      {chosen_note}
+    }}
+    \\layout {{
+      indent = 0\\mm  % Remove indentation to avoid unnecessary space
+      line-width = #150  % Adjust line width to fit your content
+      ragged-right = ##f  % To avoid ragged right lines
+      \\context {{
+        \\Score
+        \\omit TimeSignature
+        \\remove "Bar_number_engraver"  % Remove bar numbers
+      }}
+    }}
+  }}
+  """
+
+  # Write the string to a LilyPond (.ly) file
+    with open('score.ly', 'w') as f:
+        f.write(lilypond_score)
+
+    subprocess.run(['lilypond', '-dpreview', '-dbackend=eps', '--png', '-dresolution=300', '--output=score', 'score.ly'],
+                   check=True)
+  # Open the generated PNG file
+    with Image.open('score.png') as img:
+      # Calculate the crop rectangle
+      width, height = img.size
+      crop_height = height // 10 
+      crop_rectangle = (120, 0, 350, crop_height)
+      
+      # add code to crop 1/10 from the top
+      cropped_img = img.crop(crop_rectangle)
+      cropped_img.save(f'cropped_score.png')
+
+def get_note(available_clef=["treble","bass","tenor","alto"],
+             accidentals=accidentals,
+             leger_line=bool):
+    chosen_clef = random.choice(available_clef)
+    clef_data=clefs[chosen_clef]
+    chosen_accidental = random.choice(accidentals)
+    tran_acc= accidentals_lilypond[chosen_accidental]
+    if leger_line:
+        note_list=clef_data["leger_lines"]
+    else:
+        note_list=clef_data["instaff"]
+    chosen_note = random.choice(note_list)
+    octave= chosen_note[1:] if len(chosen_note)>1 else ""
+    picked_note=chosen_note[0]+tran_acc+octave
+    ranged_score_generation(chosen_clef,picked_note,clef_data['pitch_range'])
+    return chosen_clef,chosen_note[0].upper()+" "+chosen_accidental,clef_data['pitch_range']
---
+++ pitch_id/ranged_id.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import random
+from element import get_note,levels,accidentals,note_letters
+def disable_button():
+    st.session_state.pressed_id = True
+def pitch_main():
+    st.title('Note Identification')
+    col1,col2=st.columns([3,1])
+    with col1:
+        chosen_level = st.select_slider('Select the difficulty level:',options=["basic","intermediate","c clefs","all clefs"])
+    with col2:
+        st.write("  ")
+        st.write("  ")
+        ledger=st.checkbox("Use ledger lines",value=False)
+    col1,col2=st.columns(2)
+    with col1:
+        chosen_clefs= st.multiselect('Select the clef:', default=levels[chosen_level][0],options=levels[chosen_level][0])
+    with col2:
+        chosen_accidental = st.multiselect('Select the accidental:', default=levels[chosen_level][1],options=levels[chosen_level][1])
+    if len(chosen_clefs) ==0 or len(chosen_accidental)==0:
+        st.warning("Please select at least one clef")
+        st.stop()
+
+    if 'current_answer_id' not in st.session_state:
+        st.session_state.current_answer_id = None
+    if 'pressed_id' not in st.session_state:
+        st.session_state.pressed_id = True
+
+    if st.session_state.current_answer_id:
+        st.image("cropped_score.png") 
+
+    col1, col2 = st.columns(2)
+    with col1:
+        # The select box for the note letters
+        selected_note = st.selectbox('Select the note letter:', note_letters)
+        new_quest = st.button("New Question",disabled= not st.session_state.pressed_id)
+        if new_quest and st.session_state.pressed_id:
+            st.session_state.pressed_id = False
+            clef, note,acc = get_note(chosen_clefs,chosen_accidental,ledger)
+            st.session_state.current_answer_id = note
+            st.rerun()
+        
+    with col2:
+        # The select box for the accidentals
+        selected_accidental = st.selectbox('Select the accidental:', accidentals)
+        check_ans = st.button("Check Answer", on_click=disable_button,disabled=st.session_state.pressed_id)
+    user_ans = f"{selected_note}{selected_accidental}"
+
+    if check_ans:
+        if user_ans.lower() == st.session_state.current_answer_id:
+            st.success("Correct")
+        else:
+            st.error(f"The answer should be {st.session_state.current_answer_id}")
+       
+if __name__ == "__main__":
+    pitch_main()
