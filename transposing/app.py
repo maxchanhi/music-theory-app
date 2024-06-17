fabe2f459b28e6ce1b85f065185ebd86eec72d97
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-17 21:35:27 +0800

    Update app.py

--- transposing/app.py
+++ transposing/app.py
@@ -26,32 +26,33 @@ def transposing_main():
         with col1:
             st.image(f'transposing/{link}', use_column_width=True)
         with col2:
-            st.write("    ")
-            st.write("    ")
-            if st.button(f"Image {idx + 1}", key=f"btn_{idx}", on_click=disable_option_tr, args=(idx,),
+            if st.button(f"Option {idx + 1}", key=f"btn_{idx}", on_click=disable_option_tr, args=(idx,),
                          disabled=idx==st.session_state.dis_option_tr):
                 st.session_state.selected_answer_tr = idx
                 st.session_state.answer_checked_tr = False  # Reset check on new selection
     
-        # Button to check the answer
-    if st.session_state.selected_answer_tr is not None and not st.session_state.answer_checked_tr:
-        if st.button("Check answer",disabled=st.session_state.answer_checked_tr):
-            st.session_state.answer_checked_tr = True
-            if "correct" in st.session_state['img_link'][st.session_state.selected_answer_tr]:
-                st.success("Correct!")
-                rain_emoji()
-            else:
-                correct_idx = [i for i, link in enumerate(st.session_state.img_link) if "correct" in link][0] + 1
-                st.error(f"Incorrect. Image {correct_idx} is the correct answer.")
+    col1, col2 = st.columns([5, 1])
+    with col2:
+        if st.session_state.selected_answer_tr is not None and not st.session_state.answer_checked_tr:
+            check_ans = st.button("Check answer",disabled=st.session_state.answer_checked_tr)
+                
     
-    # Generate question button
-    if st.session_state.answer_checked_tr or not st.session_state.transposed_by:
-        if st.button("Generate question"):
-            st.session_state.transposed_by = picture_generation()
-            st.session_state.presses_tran = False
-            st.session_state.img_link = get_pic_link()
-            st.session_state.selected_answer_tr = None
-            st.session_state.answer_checked_tr = False
-            st.session_state.dis_option_tr=None
-            st.rerun()
+    with col1:
+        if st.session_state.answer_checked_tr or not st.session_state.transposed_by:
+            if st.button("Generate question"):
+                st.session_state.transposed_by = picture_generation()
+                st.session_state.presses_tran = False
+                st.session_state.img_link = get_pic_link()
+                st.session_state.selected_answer_tr = None
+                st.session_state.answer_checked_tr = False
+                st.session_state.dis_option_tr=None
+                st.rerun()
+    if check_ans:
+        st.session_state.answer_checked_tr = True
+        if "correct" in st.session_state['img_link'][st.session_state.selected_answer_tr]:
+            st.success("Correct!")
+            rain_emoji()
+        else:
+            correct_idx = [i for i, link in enumerate(st.session_state.img_link) if "correct" in link][0] + 1
+            st.error(f"Incorrect. Image {correct_idx} is the correct answer.")
     disclaimer()
