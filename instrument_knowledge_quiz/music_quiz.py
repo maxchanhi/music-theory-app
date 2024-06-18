4e783cd91029863d5a58069607a40a931dca176d
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-18 11:35:14 +0800

    Update music_quiz.py

--- instrument_knowledge_quiz/music_quiz.py
+++ instrument_knowledge_quiz/music_quiz.py
@@ -26,10 +26,10 @@ def knowledgemain():
         st.warning("Tip: You need to answer at least 5 questions to get the AI feedback.")
     topics = ['Reed', 'Transposing', "Clef", "Voice types", "Piano", "Ornaments", "Inst. technique"]
     topics_selected = st.multiselect('Select topics to be quizzed on:', topics, default=topics)
-    new_question = st.button('New question')
+    new_question = 
     if not topics_selected:
         st.warning('Please select at least one topic.')
-    if new_question and topics_selected and ss["pressed_kn"]:
+    def new_question_call():
         ss["pressed_kn"] = False
         test_topic = random.choice(topics_selected)
         ss["choosen_topic"] = pick_topic(test_topic)
@@ -38,13 +38,19 @@ def knowledgemain():
             ss["get_url"] = "instrument_knowledge_quiz/"+ss["choosen_topic"]["pic_url"]
         else:
             ss["get_url"] = None
+    col_1,col_2=st.columns([1,4])
+    with col_1:
+        st.button('New question',disabled= not ss["pressed_kn"],on_click=new_question_call)
+    
+    
     if ss["choosen_topic"]:
         choosen_topic = ss["choosen_topic"]
         st.subheader(choosen_topic['question'])
         if ss["get_url"]:
             st.image(ss["get_url"])
         reed_options = st.radio("Options:", choosen_topic['options'])
-        if st.button('Check Answer', disabled=ss["pressed_kn"]):
+    with col_2:
+        if st.button('Check Answer', disabled=ss["pressed_kn"] or not reed_options):
             ss["pressed_kn"] = True
             if reed_options == choosen_topic['answer']:
                 fun_emoji = random.choice(fun_emoji_list)
@@ -56,7 +62,8 @@ def knowledgemain():
                 st.error(f"Wrong! The correct answer is {choosen_topic['answer']}.")
                 ss["student_ans"].append([choosen_topic['question'], "student answer: " + reed_options, "incorrect"])
             print(ss["student_ans"])
-
+    
+    
     if len(ss["student_ans"]) > 5:
         if st.button('You can get an AI feedback'):
             with st.spinner("Generating feedback..."):
