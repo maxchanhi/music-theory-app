40be61e0264b4d79dd3a11d2a74c2ee1c90a88bd
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-07-08 23:46:52 +0800

    Add files via upload

---
+++ interval/jump_chart.py
@@ -0,0 +1,23 @@
+JUMP_CHART = {
+    ("1","1"):"Perfect",
+    ('2', '1'): "Diminished", ('2', '2'): "Minor",
+    ('2', '3'): "Major", ('2', '4'): "Augmented",
+    
+    ('3', '3'): "Diminished",('3', '4'): "Minor",
+    ('3', '5'): "Major", ('3', '6'): "Augmented",
+    
+    ('4', '5'): "Diminished",
+    ('4', '6'): "Perfect",('4', '7'): "Augmented",
+    
+    ('5', '7'): "Diminished",
+    ('5', '8'): "Perfect",('5', '9'): "Augmented",
+    
+    ('6', '8'): "Diminished",('6', '9'): "Minor",
+    ('6', '10'): "Major",('6', '11'): "Augmented",
+    
+    ('0', '10'): "Diminished",('0', '11'): "Minor",
+    ('0', '12'): "Major",('0', '13'): "Augmented",
+    
+    ('1', '12'): "Diminished",
+    ('1', '13'): "Perfect",('1', '14'): "Augmented",
+}
