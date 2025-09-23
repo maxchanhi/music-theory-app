9e3dc72239a42693440e7b75223b19bbf6507fba
Chak Hang Chan <max49363d93@gmail.com>
2025-09-23 09:14:02 +0800

    pkg

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Install system dependencies from packages.txt
-apt-get update && apt-get install -y $(cat packages.txt)
+apt-get update && apt-get install lilypond
 
 # Install python dependencies
 pip install -r requirements.txt
