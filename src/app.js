e6b713322723445b7044a582ed92862791067c2c
Chak Hang Chan <maxchanhi@users.noreply.github.com>
2026-03-01 21:47:41 +0800

    Fix: Correctly import connect-mongo for CJS environments

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default || require('connect-mongo');
 const path = require("path");
 const connectDB = require('./config/database');
 const authRouter = require('./routes/auth');
